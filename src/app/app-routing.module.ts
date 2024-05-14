7c5a291a814cbba6c2075c8171efb2d1a5b6b6f6
Mau <mauricio.chaves.chaves@est.una.ac.cr>
2024-05-14 15:15:57 -0600

    Esqueleto del componente facturacion
    ASe elimino el antiguo componente para que ahora sea listar-facturas
    que hace exactamente lo mismo pero lo acomode en otro lugar por orden
    
    ahopra la pagina principal es facturacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,26 +3,29 @@ import { RouterModule, Routes } from '@angular/router';
 import {ListarProvedoresComponent} from "./components/listar-provedores/listar-provedores.component";
 import {RegistrarUsuarioComponent} from "./components/registrar-usuario/registrar-usuario.component";
 import {LoginComponent} from "./components/login/login.component";
-import {FacturacionComponent} from "./components/facturacion/facturacion.component";
 import {AcercaDeComponent} from "./components/acerca-de/acerca-de.component";
 import {AddClienteComponent} from "./components/clientes/add-cliente/add-cliente.component";
 import {ListarClientesComponent} from "./components/clientes/listar-clientes/listar-clientes.component";
 import {AddProductoComponent} from "./components/productos/add-producto/add-producto.component";
 import { AuthGuardService } from './services/auth-guard.service';
+import {ListaFacturasComponent} from "./components/facturas/lista-facturas/lista-facturas.component";
+import {FacturarComponent} from "./components/facturas/facturar/facturar.component";
 
 const routes: Routes = [
   {path : '', redirectTo: 'login', pathMatch: 'full'} ,//si esta vacio retorna a usuarios, CAMBIAR A ALOGIN
   {path : 'login', component: LoginComponent},
   {path : 'acerca-de', component: AcercaDeComponent},
   {path : 'registrar-usuario', component: RegistrarUsuarioComponent,  },
-  {path : 'proveedores', component: ListarProvedoresComponent, canActivate: [AuthGuardService] },
-  {path : 'facturacion', component: FacturacionComponent, canActivate: [AuthGuardService] },
+  {path : 'proveedores', component: ListarProvedoresComponent, canActivate: [AuthGuardService] },//el can activate hace queno se pueda entrar si no cumple el requerimiento (en este caso estar logeado )
 
   {path : 'add-cliente', component: AddClienteComponent, canActivate: [AuthGuardService] },
   {path : 'clientes', component: ListarClientesComponent, canActivate: [AuthGuardService] },
+
   {path : 'add-producto', component: AddProductoComponent, canActivate: [AuthGuardService] },
   {path : 'productos', component: ListarProvedoresComponent, canActivate: [AuthGuardService] },
-  {path : 'facturas', component: FacturacionComponent, canActivate: [AuthGuardService] },
+
+  {path : 'facturacion', component: FacturarComponent, canActivate: [AuthGuardService] },
+  {path : 'facturas', component: ListaFacturasComponent, canActivate: [AuthGuardService] },
 
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { ListarProvedoresComponent } from './components/listar-provedores/listar-provedores.component';
 import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
 import { LoginComponent } from './components/login/login.component';
-import { FacturacionComponent } from './components/facturacion/facturacion.component';
 import { CanvasUsuarioComponent } from './components/canvas-usuario/canvas-usuario.component';
 import { ListarClientesComponent } from './components/clientes/listar-clientes/listar-clientes.component';
 import { AddClienteComponent } from './components/clientes/add-cliente/add-cliente.component';
@@ -18,6 +17,8 @@ import { AddProductoComponent } from './components/productos/add-producto/add-pr
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import {CurrentUserService} from "./services/current-user.service";
 import { AuthCurrentDirective } from './directivas/auth-current.directive';
+import { FacturarComponent } from './components/facturas/facturar/facturar.component';
+import { ListaFacturasComponent } from './components/facturas/lista-facturas/lista-facturas.component';
 
 @NgModule({
   declarations: [
@@ -26,14 +27,15 @@ import { AuthCurrentDirective } from './directivas/auth-current.directive';
     ListarProvedoresComponent,
     RegistrarUsuarioComponent,
     LoginComponent,
-    FacturacionComponent,
     CanvasUsuarioComponent,
     ListarClientesComponent,
     AddClienteComponent,
     ListaProductosComponent,
     AddProductoComponent,
     AcercaDeComponent,
-    AuthCurrentDirective
+    AuthCurrentDirective,
+    FacturarComponent,
+    ListaFacturasComponent
   ],
   imports: [
     BrowserModule,
