3872c6eddb5cdde49543e3620b268b2f658893c1
Jairo Castillo <jairoisaac20@gmail.com>
2024-05-14 19:35:31 -0600

    FindClienteByProveedorId  DONE

---
+++ src/app/services/cliente.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Cliente} from "../entities/cliente";
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+  private baseURL = 'http://localhost:8080/api/factura-electronica/clientes';
+  constructor(private httpClient:HttpClient) { }
+
+  getClientesByProveedorId(id: string): Observable<Cliente> {
+    return this.httpClient.get<Cliente>(`${this.baseURL}/${id}`);
+  }
+  
+
+
+}
