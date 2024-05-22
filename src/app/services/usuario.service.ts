9ad0a67459f14e175149e4cb3bfb061098bc0ec2
Jairo Castillo <jairoisaac20@gmail.com>
2024-05-21 20:46:15 -0600

    borre comentarios (nada mas)

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Usuario} from "../entities/usuario";
-import {Cliente} from "../entities/cliente";
+import {Cliente } from "../entities/cliente";
 
 @Injectable({
   providedIn: 'root'
